ea160cc123639b16608951faee8800a6367c49d0
RDSMatheus <matheus_rds1998@hotmail.com>
2024-01-29 13:33:28 -0300

    algumas melhorias

--- src/hashNav.ts
+++ src/hashNav.ts
@@ -1,6 +1,7 @@
 export default function hashNav(links: NodeList | null) {
   function handleClick(event: Event) {
     event.preventDefault();
+    console.log(event.target);
     if (event.target && event.target instanceof HTMLAnchorElement) {
       console.log(event.target);
       const link = event.target.getAttribute('href');
@@ -10,9 +11,20 @@ export default function hashNav(links: NodeList | null) {
   }
 
   if (links && links.length) {
+    console.log(links);
     links.forEach((link) => {
       if (link instanceof HTMLAnchorElement)
         link.addEventListener('click', handleClick);
+    });
+  }
+
+  const logo = document.querySelector('.logo') as HTMLImageElement | null;
+  if (logo) {
+    logo.addEventListener('click', (event) => {
+      event.preventDefault();
+      const link = '#home';
+      const element = link ? document.querySelector(link) : null;
+      element?.scrollIntoView({ behavior: 'smooth' });
     });
   }
 }
