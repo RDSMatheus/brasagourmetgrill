2598107b5c54d673753a2ecf0342a39f076ca748
RDSMatheus <matheus_rds1998@hotmail.com>
2024-02-02 20:29:30 -0300

    fix scroll mobile

--- src/menuTouch.ts
+++ src/menuTouch.ts
@@ -1,24 +1,31 @@
-// export default function menuTouch(container: Element | null) {
-//   function handleTouch(event: TouchEvent, items: NodeList) {
-//     event.preventDefault();
-//     items.forEach((item) => {
-//       if (item instanceof HTMLElement) {
-//         item.classList.remove('ativo');
-//       }
-//     });
-//     const targetElement = event.target as HTMLElement;
-//     targetElement.classList.add('ativo');
-//     setTimeout(() => {
-//       targetElement.classList.remove('ativo');
-//     }, 4000);
-//   }
+export default function menuTouch(container: Element | null) {
+  let timeout: NodeJS.Timeout | null = null;
 
-//   if (container) {
-//     const items = container.querySelectorAll('div');
-//     items?.forEach((item) =>
-//       item.addEventListener('touchend', (event) => {
-//         handleTouch(event, items);
-//       }),
-//     );
-//   }
-// }
+  function handleTouch(event: TouchEvent, items: NodeList) {
+    event.preventDefault();
+    items.forEach((item) => {
+      if (item instanceof HTMLElement) {
+        item.classList.remove('ativo');
+      }
+    });
+    const targetElement = event.target as HTMLElement;
+    targetElement.classList.add('ativo');
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      targetElement.classList.remove('ativo');
+    }, 4000);
+  }
+
+  if (container) {
+    const items = container.querySelectorAll('div');
+    items?.forEach((item) =>
+      item.addEventListener('touchend', (event) => {
+        handleTouch(event, items);
+      }),
+    );
+  }
+}
