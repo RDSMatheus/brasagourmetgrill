b6d9751ce99e388511cfbbbf6f542abb3da7f710
RDSMatheus <matheus_rds1998@hotmail.com>
2024-02-02 19:30:59 -0300

    fix scroll mobile

--- src/menuTouch.ts
+++ src/menuTouch.ts
@@ -1,24 +1,24 @@
-export default function menuTouch(container: Element | null) {
-  function handleTouch(event: TouchEvent, items: NodeList) {
-    event.preventDefault();
-    items.forEach((item) => {
-      if (item instanceof HTMLElement) {
-        item.classList.remove('ativo');
-      }
-    });
-    const targetElement = event.target as HTMLElement;
-    targetElement.classList.add('ativo');
-    setTimeout(() => {
-      targetElement.classList.remove('ativo');
-    }, 4000);
-  }
+// export default function menuTouch(container: Element | null) {
+//   function handleTouch(event: TouchEvent, items: NodeList) {
+//     event.preventDefault();
+//     items.forEach((item) => {
+//       if (item instanceof HTMLElement) {
+//         item.classList.remove('ativo');
+//       }
+//     });
+//     const targetElement = event.target as HTMLElement;
+//     targetElement.classList.add('ativo');
+//     setTimeout(() => {
+//       targetElement.classList.remove('ativo');
+//     }, 4000);
+//   }
 
-  if (container) {
-    const items = container.querySelectorAll('div');
-    items?.forEach((item) =>
-      item.addEventListener('touchend', (event) => {
-        handleTouch(event, items);
-      }),
-    );
-  }
-}
+//   if (container) {
+//     const items = container.querySelectorAll('div');
+//     items?.forEach((item) =>
+//       item.addEventListener('touchend', (event) => {
+//         handleTouch(event, items);
+//       }),
+//     );
+//   }
+// }
